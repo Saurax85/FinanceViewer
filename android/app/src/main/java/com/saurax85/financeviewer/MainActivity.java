9cfd2df68ed7d6d0248def64c81de231406f49f2
Saurax85 <manuel.fox.volpe@gmail.com>
2026-09-11 17:30:47 +0200

    Add files via upload

--- android/app/src/main/java/com/saurax85/financeviewer/MainActivity.java
+++ android/app/src/main/java/com/saurax85/financeviewer/MainActivity.java
@@ -106,9 +106,9 @@ public class MainActivity extends Activity {
         String js = "(function(){"
                 + "var m=document.querySelector('main');"
                 + "if(m){"
-                + "m.style.setProperty('transform','scale(0.95)','important');"
+                + "m.style.setProperty('transform','scale(0.70)','important');"
                 + "m.style.setProperty('transform-origin','top left','important');"
-                + "m.style.setProperty('width','105.263158%','important');"
+                + "m.style.removeProperty('width');"
                 + "}"
                 + "})();";
         view.evaluateJavascript(js, null);
