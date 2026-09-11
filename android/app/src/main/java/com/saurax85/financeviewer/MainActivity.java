9defa7fceef5258f3485cd0b8a461c94c00141c1
Saurax85 <manuel.fox.volpe@gmail.com>
2026-09-11 15:25:22 +0200

    Add files via upload

--- android/app/src/main/java/com/saurax85/financeviewer/MainActivity.java
+++ android/app/src/main/java/com/saurax85/financeviewer/MainActivity.java
@@ -1,9 +1,12 @@
 package com.saurax85.financeviewer;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowInsets;
 import android.webkit.CookieManager;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
@@ -11,14 +14,18 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class MainActivity extends Activity {
-    private static final String FINANCE_VIEWER_URL =
-        "https://script.google.com/macros/s/AKfycbxMnUJiO1mRz0VRRA_5qnmyb-GANxy5US5d7KoabAodtGtlpRsNAbaOGR6nXeQ651tM/exec?action=2026&mobile=1&refresh=1";
 
     private WebView webView;
 
+    private static final String URL =
+            "https://script.google.com/macros/s/AKfycbxMnUJiO1mRz0VRRA_5qnmyb-GANxy5US5d7KoabAodtGtlpRsNAbaOGR6nXeQ651tM/exec"
+            + "?action=2026&mobile=1&refresh=1";
+
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         webView = new WebView(this);
         webView.setBackgroundColor(Color.rgb(11, 15, 20));
         setContentView(webView);
@@ -27,21 +34,47 @@ public class MainActivity extends Activity {
         settings.setJavaScriptEnabled(true);
         settings.setDomStorageEnabled(true);
         settings.setDatabaseEnabled(true);
-        settings.setBuiltInZoomControls(false);
-        settings.setDisplayZoomControls(false);
-        settings.setLoadWithOverviewMode(true);
-        settings.setUseWideViewPort(true);
-        settings.setTextZoom(90);
-        settings.setSupportMultipleWindows(false);
-        settings.setMediaPlaybackRequiresUserGesture(false);
+        settings.setAllowFileAccess(false);
+        settings.setAllowContentAccess(false);
 
-        CookieManager.getInstance().setAcceptCookie(true);
-        CookieManager.getInstance().setAcceptThirdPartyCookies(webView, true);
+        // Keep the existing Mobile layout scale as close as possible.
+        settings.setUseWideViewPort(true);
+        settings.setLoadWithOverviewMode(true);
+        settings.setTextZoom(85);
+
+        CookieManager cookieManager = CookieManager.getInstance();
+        cookieManager.setAcceptCookie(true);
+        cookieManager.setAcceptThirdPartyCookies(webView, true);
 
         webView.setWebViewClient(new WebViewClient());
         webView.setWebChromeClient(new WebChromeClient());
-        webView.setOverScrollMode(View.OVER_SCROLL_NEVER);
-        webView.loadUrl(FINANCE_VIEWER_URL);
+
+        webView.setOverScrollMode(WebView.OVER_SCROLL_NEVER);
+        webView.setHorizontalScrollBarEnabled(false);
+        webView.setHorizontalFadingEdgeEnabled(false);
+
+        // Android 15 / targetSdk 35 enforces edge-to-edge. Keep the
+        // FinanceViewer content below the status/camera area and above
+        // the navigation area, without changing Mobile.html.
+        webView.setOnApplyWindowInsetsListener((view, insets) -> {
+            int top;
+            int bottom;
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                top = insets.getInsets(WindowInsets.Type.statusBars()
+                        | WindowInsets.Type.displayCutout()).top;
+                bottom = insets.getInsets(WindowInsets.Type.navigationBars()).bottom;
+            } else {
+                top = insets.getSystemWindowInsetTop();
+                bottom = insets.getSystemWindowInsetBottom();
+            }
+
+            view.setPadding(0, top, 0, bottom);
+            return insets;
+        });
+
+        webView.post(() -> webView.requestApplyInsets());
+        webView.loadUrl(URL);
     }
 
     @Override
@@ -51,14 +84,5 @@ public class MainActivity extends Activity {
         } else {
             super.onBackPressed();
         }
-    }
-
-    @Override
-    protected void onDestroy() {
-        if (webView != null) {
-            webView.stopLoading();
-            webView.destroy();
-        }
-        super.onDestroy();
     }
 }
