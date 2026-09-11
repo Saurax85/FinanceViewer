befddeca79bbca5151a23053ee88b4d5780fb4dc
Saurax85 <manuel.fox.volpe@gmail.com>
2026-09-11 14:17:48 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,30 @@
-const CACHE='financeviewer-pwa-v1';
+const CACHE='financeviewer-pwa-v2';
 const APP=['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(APP)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+
+self.addEventListener('install',e=>
+  e.waitUntil(
+    caches.open(CACHE)
+      .then(c=>c.addAll(APP))
+      .then(()=>self.skipWaiting())
+  )
+);
+
+self.addEventListener('activate',e=>
+  e.waitUntil(
+    caches.keys()
+      .then(keys=>Promise.all(
+        keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))
+      ))
+      .then(()=>self.clients.claim())
+  )
+);
+
 self.addEventListener('fetch',e=>{
   const u=new URL(e.request.url);
+
   if(u.origin===self.location.origin){
-    e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+    e.respondWith(
+      caches.match(e.request).then(r=>r||fetch(e.request))
+    );
   }
 });
