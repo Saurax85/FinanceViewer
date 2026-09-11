1a83885c70af7af57872a3965ccac8fce47d86c2
Saurax85 <manuel.fox.volpe@gmail.com>
2026-09-11 15:16:36 +0200

    Add files via upload

--- android/app/src/main/java/com/saurax85/financeviewer/MainActivity.java
+++ android/app/src/main/java/com/saurax85/financeviewer/MainActivity.java
@@ -1,51 +1,47 @@
 package com.saurax85.financeviewer;
 
-import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.view.KeyEvent;
+import android.view.View;
 import android.webkit.CookieManager;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
-import android.app.Activity;
-
 public class MainActivity extends Activity {
+    private static final String FINANCE_VIEWER_URL =
+        "https://script.google.com/macros/s/AKfycbxMnUJiO1mRz0VRRA_5qnmyb-GANxy5US5d7KoabAodtGtlpRsNAbaOGR6nXeQ651tM/exec?action=2026&mobile=1&refresh=1";
 
     private WebView webView;
 
-    private static final String URL =
-            "https://script.google.com/macros/s/AKfycbxMnUJiO1mRz0VRRA_5qnmyb-GANxy5US5d7KoabAodtGtlpRsNAbaOGR6nXeQ651tM/exec"
-            + "?action=2026&mobile=1&refresh=1";
-
-    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         webView = new WebView(this);
+        webView.setBackgroundColor(Color.rgb(11, 15, 20));
         setContentView(webView);
 
         WebSettings settings = webView.getSettings();
-
         settings.setJavaScriptEnabled(true);
         settings.setDomStorageEnabled(true);
         settings.setDatabaseEnabled(true);
-        settings.setAllowFileAccess(false);
-        settings.setAllowContentAccess(false);
+        settings.setBuiltInZoomControls(false);
+        settings.setDisplayZoomControls(false);
+        settings.setLoadWithOverviewMode(true);
+        settings.setUseWideViewPort(true);
+        settings.setTextZoom(90);
+        settings.setSupportMultipleWindows(false);
+        settings.setMediaPlaybackRequiresUserGesture(false);
 
-        CookieManager cookieManager = CookieManager.getInstance();
-        cookieManager.setAcceptCookie(true);
-        cookieManager.setAcceptThirdPartyCookies(webView, true);
+        CookieManager.getInstance().setAcceptCookie(true);
+        CookieManager.getInstance().setAcceptThirdPartyCookies(webView, true);
 
         webView.setWebViewClient(new WebViewClient());
         webView.setWebChromeClient(new WebChromeClient());
-
-        webView.setOverScrollMode(WebView.OVER_SCROLL_NEVER);
-        webView.setHorizontalScrollBarEnabled(false);
-
-        webView.loadUrl(URL);
+        webView.setOverScrollMode(View.OVER_SCROLL_NEVER);
+        webView.loadUrl(FINANCE_VIEWER_URL);
     }
 
     @Override
@@ -58,7 +54,11 @@ public class MainActivity extends Activity {
     }
 
     @Override
-    public boolean dispatchKeyEvent(KeyEvent event) {
-        return super.dispatchKeyEvent(event);
+    protected void onDestroy() {
+        if (webView != null) {
+            webView.stopLoading();
+            webView.destroy();
+        }
+        super.onDestroy();
     }
 }
