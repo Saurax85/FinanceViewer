761af71d81e5cf24cc6357aaef9b481c83cf81e0
Saurax85 <manuel.fox.volpe@gmail.com>
2026-09-11 14:04:47 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,10 @@
+const CACHE='financeviewer-pwa-v1';
+const APP=['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(APP)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>{
+  const u=new URL(e.request.url);
+  if(u.origin===self.location.origin){
+    e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+  }
+});
